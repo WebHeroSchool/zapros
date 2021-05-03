23d7eeceb9bb52b84f8bff99b2b5fc3e5702b385
Соломон Захаров <solomon7and@gmail.com>
2021-05-03 21:33:36 +0500

    получение и вывод

---
+++ fetch.js
@@ -0,0 +1,35 @@
+let url = 'https://api.github.com/users/Solomon7and7';
+
+async function getProfileInfo() {
+
+  let profile = await fetch(url)
+  if (profile.ok) {
+    let json = await profile.json()
+
+    let avatar = json.avatar_url;
+    let name = json.name;
+    let bio = json.bio;
+    let html = json.html_url;
+
+    let list = document.querySelector('.profile')
+
+    list.innerHTML += `
+      <img src="${json.avatar_url}" alt="avatar">
+      <div><a href="${json.html_url}">${json.name}</a></div>
+      <p>${json.bio}</p>
+    `
+
+
+    console.log(name, avatar, bio, html)
+  } else {
+      let list = document.querySelector('.profile')
+
+      list.innerHTML += `
+        <h3>Информация о пользователе не доступна</h3>
+      `
+      alert('Информация о пользователе не доступна')
+      console.log('Информация о пользователе не доступна')
+  }
+}
+
+getProfileInfo()
