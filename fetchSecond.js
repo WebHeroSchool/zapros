d998b3c89760a0e22c968ee05b785bf924fc15bb
Соломон Захаров <solomon7and@gmail.com>
2021-05-04 20:00:13 +0500

    лишнее убрал

--- fetchSecond.js
+++ fetchSecond.js
@@ -1,6 +1,5 @@
 fetch('https://api.github.com/users/Solomon7and7')
   .then(res=>res.json())
-  // let list = document.querySelector('.profile')
   .then(
     json=>document.querySelector('.profile').innerHTML+=`
       <img src="${json.avatar_url}" alt="avatar">
